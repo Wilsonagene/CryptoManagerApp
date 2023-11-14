c0e2f0436d3d1cc9b62742710cf78ec142d2c9ec
wilson agene <wilsonagene607@gmail.com>
2023-11-14 15:02:13 +0100

    Pagenation And Infinity Scroll Added

---
+++ WilsonCryptoApp/Core/Home/View/Settings/LoaderView.swift
@@ -0,0 +1,18 @@
+//
+//  LoaderView.swift
+//  WilsonCryptoApp
+//
+//  Created by Emmanuel Agene on 14/11/2023.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoaderView()
+}
